d03dcc2f18d716e0d73759a115d62f3d6991da6d
Sergey Sokol <serg607.sokol@gmail.com>
2016-06-16 11:25:05 +0300

    js_dz

--- dz_JavaScript/second_dz/two_array_with_the_numbers_randomly.js
+++ dz_JavaScript/second_dz/two_array_with_the_numbers_randomly.js
@@ -1,9 +1,9 @@
 //Создать массив и заполнить его десятью значениями в пределах -100...100 при помощи Math.random. Возвести каждый элемент массива в квадрат и вернуть массив, который будет содержать элементы > 100;
  generation = function(max,min) { // Функция генерации чисел
   max = 100;
-  min = -100
+  min = -100;
  return Math.floor(Math.random() * (max - min)) + min;
-};
+}
 
 arr = [] // создаем массив в котором генерируются 10 значений
 inArr = function(val) {
@@ -11,15 +11,13 @@ inArr = function(val) {
    if(arr[i] === val) 
      return true;
  }
-};
+}
 
 while(arr.length<10) { // Массив состоит из 10 значений
  tmp = generation()
 if(inArr(tmp)) continue; 
  arr.push(tmp)
 }
-console.log(arr);
-
 
 arr2 = [] // создаем массив в который будут возращатся возведенные в квадрат значения >100 
 var length = arr.length;
@@ -31,4 +29,33 @@ for(var i = 0; i < length; i++) {
     arr2.push(num);
   }
 }
+
+console.log(arr);
 console.log(arr2);
+
+
+//Создать массив и заполнить его десятью значениями в пределах -100...100 при помощи Math.random. Возвести каждый элемент массива в квадрат и вернуть массив, который будет содержать элементы > 100;
+var arr = [],
+arr2 = [],
+min = -100,
+max = 100,
+number = 10;
+
+function generate(min,max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+for(i = 0; i < number, i++) {
+  arr.push(generate(min,max))
+}
+
+for (var i = 0; i < arr.length; i++) {
+  arr[i] = arr[i] * arr[i];
+  if (arr[i] > 100) {
+    arr2.push(arr[i]);
+  }
+}
+
+console.log(arr);
+console.log(arr2);
+
