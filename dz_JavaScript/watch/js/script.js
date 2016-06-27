2a371d7ac2fdcc06a3b353d59c513fe956f8baad
Sergey Sokol <serg607.sokol@gmail.com>
2016-06-27 20:15:55 +0300

    watch

--- dz_JavaScript/watch/js/script.js
+++ dz_JavaScript/watch/js/script.js
@@ -1,11 +1,25 @@
 var buttons = document.getElementsByTagName ("button");
+var timer;
 
-buttons[0].addEventListener("click", time);
-function time(e) {
+buttons[0].addEventListener("click", start);
+buttons[1].addEventListener("click", reset);
+document.getElementById("watch").innerHTML =  "00:00:00" // нулевой счетчик
+
+function start() { // функция запускает часы
+  time();
+  timer = setInterval(time, 1000);
+};
+
+function reset() { // функция сбрасывает время
+  window.clearInterval(timer);
+  document.getElementById("watch").innerHTML =  "00:00:00"
+};
+
+function time() { // функция самих часов
 	var d = new Date();
 	document.getElementById("watch").innerHTML = d.toLocaleTimeString();
-	};
-setInterval("time(e)", 1000);
+};
+
 
 
 
