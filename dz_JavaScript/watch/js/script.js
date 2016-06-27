74cd971ba3fd93ed755c6bbdd6101108d6f630c0
Sergey Sokol <serg607.sokol@gmail.com>
2016-06-27 20:57:23 +0300

    watch

--- dz_JavaScript/watch/js/script.js
+++ dz_JavaScript/watch/js/script.js
@@ -5,6 +5,11 @@ buttons[0].addEventListener("click", start);
 buttons[1].addEventListener("click", reset);
 document.getElementById("watch").innerHTML =  "00:00:00" // нулевой счетчик
 
+function time() { // функция самих часов
+	var d = new Date();
+	document.getElementById("watch").innerHTML = d.toLocaleTimeString();
+};
+
 function start() { // функция запускает часы
   time();
   timer = setInterval(time, 1000);
@@ -15,10 +20,23 @@ function reset() { // функция сбрасывает время
   document.getElementById("watch").innerHTML =  "00:00:00"
 };
 
-function time() { // функция самих часов
-	var d = new Date();
-	document.getElementById("watch").innerHTML = d.toLocaleTimeString();
-};
+// сeкундомер
+var start = document.querySelector('.start');
+var stop = document.querySelector('.stop');
+var reset = document.querySelector('.reset');
+var lap = document.querySelector('.lap');
+
+document.getElementById("stopw").innerHTML =  "00:00:00"
+
+start.addEventListener("click", startSec);
+stop.addEventListener("click", stopSec);
+reset.addEventListener("click", resetSec);
+lap.addEventListener("click", lapSec);
+
+
+
+
+
 
 
 
