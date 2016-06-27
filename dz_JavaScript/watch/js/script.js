a6e541335ec8eaf20b081373ae215618951501ed
Sergey Sokol <serg607.sokol@gmail.com>
2016-06-27 19:16:39 +0300

    watch

--- dz_JavaScript/watch/js/script.js
+++ dz_JavaScript/watch/js/script.js
@@ -1,11 +1,12 @@
-window.onload = function() {
-   window.setInterval (
-	function() {
-	    var d = new Date();
-	    document.getElementById("watch").innerHTML = d.toLocaleTimeString();
-	}
-  , 1000);
-}
+var buttons = document.getElementsByTagName ("button");
+
+buttons[0].addEventListener("click", time);
+function time(e) {
+	var d = new Date();
+	document.getElementById("watch").innerHTML = d.toLocaleTimeString();
+	};
+setInterval("time(e)", 1000);
+
 
 
 
