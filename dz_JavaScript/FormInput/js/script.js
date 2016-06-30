22cd8db5ef463b8193eff96db80397e3d8ffaa45
Sergey Sokol <serg607.sokol@gmail.com>
2016-06-30 16:31:14 +0300

    dz

--- dz_JavaScript/FormInput/js/script.js
+++ dz_JavaScript/FormInput/js/script.js
@@ -2,19 +2,38 @@ var form = document.forms.add;
 
 form.onsubmit = function(e) {
     e.preventDefault();
-    var number;
     var result;
-
-    number = document.getElementById("age").value;
-
-    if (isNaN(number) || number < 1 || number > 99) {
-        result = "Неверный возраст";
-    } else {
-        result = " ";
+    
+    function age() {
+        var number;
+        number = document.getElementById("age").value;
+        if (isNaN(number) || number < 1 || number > 99) {
+            result = "Неверный возраст";
+        } else {
+            result = " ";
+        }
+        document.getElementById('result').innerHTML = result;
     }
-    document.getElementById('result').innerHTML = result;
+    age();
+
+    function email() {
+        var regexp = /^.+@.+\..+$/ ;
+        var email;
+        email = document.getElementById("email").value;
+        if (regexp.test(email) != -1) {
+            result = "Неверный адресс почты";
+        } else {
+            result = " ";
+        }
+        document.getElementById('result').innerHTML = result;
+    }
+    email();
 }
 this.submit();
+
+
+
+
 
 
 
