30d80097d919620ccee922dea2b16ea435569081
Sergey Sokol <serg607.sokol@gmail.com>
2016-07-03 20:28:39 +0300

    js

--- dz_JavaScript/FormInput/js/script.js
+++ dz_JavaScript/FormInput/js/script.js
@@ -1,66 +1,32 @@
-var form = document.forms.add;
-var inputs = document.getElementsByTagName('input');
-for (var i = 0; i < inputs.length; i++) {
-    inputs[i] = form.onsubmit;
-}
-
-    
-form.onsubmit = function age() {
-    var number; 
-    var result;
-    number = +document.getElementById("age").value;
-        if (isNaN(number) || number < 12 || number > 80) {
-            result = "Неверный возраст";
-            document.getElementById('result').innerHTML = result; 
-        }
-        return !result;
+var validation = {
+    name: /^[а-яёА-ЯЁ-]{2,}$/,
+    surname: /^[а-яёА-ЯЁ-]{2,}$/,
+    age: /^\d{1,3}$/,
+    email: /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/,
+    gender: /(male|famale)/
 };
 
-form.onsubmit = function name() {
-    var name; 
-    var result2;
-    num = document.getElementById("name").value;
-        if (name == null) {
-            result2 = "Ошибка ввода имени";
-            document.getElementById('result').innerHTML = result2;
-        }
-        return !result2;
-}
+var form = document.getElementsByTagName('form');
+var result = document.getElementById('result');
 
-/*
-var form = document.forms.add;
 form.onsubmit = function(e) {
     e.preventDefault();
-    var result;
-    
-    function age() {
-        var number;
-        number = document.getElementById("age").value;
-        if (isNaN(number) || number < 1 || number > 99) {
-            result = "Неверный возраст";
-        } else {
-            result = " ";
-        }
-        document.getElementById('result').innerHTML = result;
+    if (!validateForm()) {
+        return;
     }
-    age();
-    this.submit()
+    alert("Данные введены правильно, форма отправляется");
+    this.submit();
+};
+
+function validateForm() {
+    for (var field in validation) {
+        if (!validation[field].test(form.elements[field].value)) {
+            result.innerHTML = "Ошибка ввода, поле " + field;
+            return false; 
+        }
+    }
+    return true;
 }
-
-function email() {
-        var regexp = /^.+@.+\..+$/ ;
-        var email;
-        email = document.getElementById("email").value;
-        if (regexp.test(email) != -1) {
-            result = "Неверный адресс почты";
-        } else {
-            result = " ";
-        }
-        document.getElementById('result').innerHTML = result;
-    }
-    email();
-*/
-
 
 
 
