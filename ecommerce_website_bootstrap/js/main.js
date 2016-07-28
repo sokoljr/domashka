1ca029f75605e281173c3a9c73cdef7c2b5d1a14
sokoljr <Serg607.sokol@gmail.com>
2016-07-29 00:55:25 +0300

    complete second slider_slick

--- ecommerce_website_bootstrap/js/main.js
+++ ecommerce_website_bootstrap/js/main.js
@@ -1,5 +1,13 @@
-$('.second_slider_container').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1
+$(document).ready(function(){
+	$('#slick_slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		speed: 1000,
+		focusOnSelect: true,
+		prevArrow: $(".second_slider_container .second_slider_controls.left"),
+		nextArrow: $(".second_slider_container .second_slider_controls.right"),
+	});
+	$(".second_slider_container .second_slider_controls")
 });
